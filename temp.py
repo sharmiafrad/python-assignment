711726dd5b86a66a8c2afdd8461e63a311c2c1e8
sharmiafrad <tahminaafradsharmi@gmail.com>
2025-01-10 22:20:18 +0100

    Main file added

---
+++ temp.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+data = pd.read_csv('D:/Climate Data Bangladesh/Maximum_Temperature_Report.csv' , sep = "\t")
